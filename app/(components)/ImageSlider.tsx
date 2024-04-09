479eee18fe360656bee6b010d4b884f9ade29ffe
linarydholm <lina.rydholm@gmail.com>
2024-04-09 12:16:06 +0200

    slider with accessibility features in mind

--- app/(components)/ImageSlider.tsx
+++ app/(components)/ImageSlider.tsx
@@ -1,7 +1,8 @@
 'use client';
+
 // https://www.npmjs.com/package/tailwind-cn
 import { cn } from 'tailwind-cn';
-import { CornerDownLeft, CornerDownRight } from 'lucide-react';
+import { Circle, CircleDot, CornerDownLeft, CornerDownRight } from 'lucide-react';
 import { useState } from 'react';
 import Image from 'next/image';
 import { ImageProps } from '../page';
@@ -12,7 +13,6 @@ type ImagesProps = {
 
 export function ImageSlider(props: ImagesProps) {
   const { images } = props;
-
   const [imageIndex, setImageIndex] = useState(0);
 
   function showPrevImage() {
@@ -30,60 +30,92 @@ export function ImageSlider(props: ImagesProps) {
   }
 
   return (
-    <div className="w-full h-full relative">
-      <div className="w-full h-full flex overflow-hidden">
-        {images.map((image: ImageProps) => {
-          return (
-            // image should always be the same size and not shrink or grow: shrink-0 grow-0
-            <Image
-              alt=""
-              className="object-cover w-full h-full block shrink-0 grow-0"
-              key={image.src}
-              src={image.src}
-              width={image.width}
-              height={image.height}
-              // in this style we move all our images 100% to the left when we click
-              style={{ translate: `${-100 * imageIndex}%` }}
-            />
-          );
-        })}
-      </div>
-
-      <button
-        aria-label="show previous image"
-        onClick={showPrevImage}
-        className="absolute p-2 block top-0 bottom-0 left-0 bg-red-900 bg-opacity-30 hover:bg-opacity-50 transition-colors"
+    <>
+      <a
+        href="#after-image-slider"
+        className="absolute w-[1px] h-[1px] m-[-1px] focus-visible:top-4 focus-visible:left-4 focus-visible:w-auto focus-visible:h-auto focus-visible:p-2 focus-visible:z-50 focus-visible:bg-black focus-visible:text-white"
       >
-        {/* what is stroke-white and fill-black */}
-        <CornerDownLeft className="stroke-white w-8 h-8" />
-      </button>
+        Skip image slider controls
+      </a>
 
-      <button
-        aria-label="show next image"
-        onClick={showNextImage}
-        className="absolute p-2 block top-0 bottom-0 right-0 bg-red-900 bg-opacity-30 hover:bg-opacity-50 transition-colors"
+      <section
+        aria-label="Image slider"
+        className="w-full h-full relative"
       >
-        <CornerDownRight className="stroke-white w-8 h-8" />
-      </button>
+        <div className="w-full h-full flex overflow-hidden">
+          {images.map(({ image, alt }: ImageProps, index) => {
+            return (
+              // image should always be the same size and not shrink or grow: shrink-0 grow-0
+              <Image
+                aria-hidden={imageIndex !== index}
+                alt={alt}
+                className="object-cover w-full h-full block shrink-0 grow-0"
+                key={image.src}
+                src={image.src}
+                width={image.width}
+                height={image.height}
+                // in this style we move all our images 100% to the left when we click
+                // how do I do this with tailwind css?
+                style={{ translate: `${-100 * imageIndex}%` }}
+              />
+            );
+          })}
+        </div>
 
-      <div className="absolute bottom-0 flex gap-6 left-1/2 -translate-x-1/2 mb-4 mx-auto">
-        {images.map((_image: ImageProps, index: number) => {
-          const cnClass = cn(
-            'text-gray-400 bg-white bg-opacity-75 hover:bg-opacity-85 h-9 w-9 rounded hover:text-red-500 transition-colors',
-            index + 1 === imageIndex + 1 && 'text-red-500 bg-opacity-85'
-          );
+        <button
+          aria-label="show previous image"
+          onClick={showPrevImage}
+          className="absolute p-2 block top-0 bottom-0 left-0 bg-red-900 bg-opacity-30 hover:bg-opacity-50 focus-visible:bg-opacity-50 transition-colors"
+        >
+          {/* what is stroke-white and fill-black */}
+          <CornerDownLeft
+            aria-hidden={true}
+            className="stroke-white w-8 h-8"
+          />
+        </button>
 
-          return (
-            <button
-              key={index}
-              className={cnClass}
-              onClick={() => setImageIndex(index)}
-            >
-              {index + 1}
-            </button>
-          );
-        })}
-      </div>
-    </div>
+        <button
+          aria-label="show next image"
+          onClick={showNextImage}
+          className="absolute p-2 block top-0 bottom-0 right-0 bg-red-900 bg-opacity-30 hover:bg-opacity-50 focus-visible:bg-opacity-50 transition-colors"
+        >
+          <CornerDownRight
+            aria-hidden={true}
+            className="stroke-white w-8 h-8"
+          />
+        </button>
+
+        <div className="absolute bottom-0 flex gap-6 left-1/2 -translate-x-1/2 mb-4 mx-auto">
+          {images.map((_image: ImageProps, index: number) => {
+            return (
+              <button
+                aria-label={`View image ${index + 1}`}
+                key={index}
+                className={cn(
+                  'h-full w-full flex justify-center items-center focus-visible:scale-110 group transition-transform duration-200',
+                  index === imageIndex && 'opacity-90'
+                )}
+                onClick={() => setImageIndex(index)}
+              >
+                {index === imageIndex ? (
+                  // använd aria-hidden="true" för att dölja image"" i accessibility-trädet för varje ikon som renderas
+                  <CircleDot
+                    aria-hidden="true"
+                    className="fill-red-900 stroke-white opacity-95 hover:scale-110 transition-transform duration-200"
+                  />
+                ) : (
+                  <Circle
+                    aria-hidden="true"
+                    className="fill-red-900 stroke-white opacity-60 hover:opacity-95 hover:scale-110 transition-transform duration-200 group-focus-visible:opacity-95"
+                  />
+                )}
+              </button>
+            );
+          })}
+        </div>
+      </section>
+
+      <div id="after-image-slider" />
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import tyler from '@/app/(img)/tyler-clemmensen-4gSavS9pe1s-unsplash.jpg';
 // components
 import { ImageSlider } from './(components)/ImageSlider';
 
-export type ImageProps = {
+export type DataFromImage = {
   src: string;
   height: number;
   width: number;
@@ -17,13 +17,30 @@ export type ImageProps = {
   blurHeight?: number;
 };
 
+export type ImageProps = {
+  image: DataFromImage;
+  alt: string;
+};
+
 // variables
-const images: ImageProps[] = [chris, dhiva, josh, marek, tyler];
+const images: ImageProps[] = [
+  { image: chris, alt: 'Bil 1' },
+  { image: dhiva, alt: 'Bil 2' },
+  { image: josh, alt: 'Bil 3' },
+  { image: marek, alt: 'Bil 4' },
+  { image: tyler, alt: 'Bil 5' },
+];
 
 export default function App() {
   return (
     <div className="max-w-2xl w-1/2 aspect-[4/5] mx-auto my-0">
       <ImageSlider images={images} />
+      <a
+        href="/"
+        className="text-3xl"
+      >
+        Link
+      </a>
     </div>
   );
 }
